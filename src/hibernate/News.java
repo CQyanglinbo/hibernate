691fe68cc53d6103ec1d20c5bcfcda87702cafaf
CQyanglinbo <1044450759@qq.com>
2017-10-11 16:07:22 +0800

    hibernate配置文件以及类的提交

---
+++ src/hibernate/News.java
@@ -0,0 +1,44 @@
+package hibernate;
+import java.util.Date;
+public class News {
+	public News() {
+		
+	}
+	public News(String title, String content, Date date) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.date = date;
+	}
+	private Integer id;
+	private String title;
+	private String content;
+	private Date date;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String toString(){
+		return "News[id="+id+",title="+title+",content="+content+",date="+date+"]";
+	}
+}
